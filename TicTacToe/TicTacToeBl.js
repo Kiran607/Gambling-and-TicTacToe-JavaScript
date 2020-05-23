41eb14c0db4c1cf6000915c0991463c53b6430d4
Kiran607 <ickirankiru@gmail.com>
2020-05-23 12:10:55 +0530

    [Kiran]Added function to know the player symbols

--- TicTacToe/TicTacToeBl.js
+++ TicTacToe/TicTacToeBl.js
@@ -1,9 +1,21 @@
-let gameBoard=[];
+let gameBoard = [];
 let player;
 let gameStatus;
+let letter="";
 
-reset=()=>{
-    gameBoard=['.','.','.','.','.','.','.','.','.'];
-    player=1;
-    gameStatus=1;
+reset = () => {
+    gameBoard = ['.', '.', '.', '.', '.', '.', '.', '.', '.'];
+    player = 1;
+    gameStatus = 1;
+}
+
+ticTacToeMain = () => {
+    if(player==1){
+        letter="O";
+        console.log("Player player1 trun:",letter);
+        
+    }else{
+        letter="X";
+        console.log("Player player2 trun:",letter);
+    }
 }
