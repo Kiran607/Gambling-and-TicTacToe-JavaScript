cf6e7a26357a7facee98ce7545a629ec9c70eb8b
Kiran607 <ickirankiru@gmail.com>
2020-05-23 13:01:40 +0530

    [Kiran]Added board so player can see and choose the spot

--- TicTacToe/TicTacToeBl.js
+++ TicTacToe/TicTacToeBl.js
@@ -1,7 +1,11 @@
 let gameBoard = [];
 let player;
 let gameStatus;
-let letter="";
+let letter = "";
+let input = require('readline-sync');
+let selectInput;
+let row;
+let column;
 
 reset = () => {
     gameBoard = ['.', '.', '.', '.', '.', '.', '.', '.', '.'];
@@ -9,13 +13,89 @@ reset = () => {
     gameStatus = 1;
 }
 
+print = () => {
+    console.log("r\c  0 1 2");
+    console.log("0   ", gameBoard[0], gameBoard[1], gameBoard[2]);
+    console.log("1   ", gameBoard[3], gameBoard[4], gameBoard[5]);
+    console.log("2   ", gameBoard[6], gameBoard[7], gameBoard[8]);
+}
+
+set = (rows, columns, input) => {
+    let index = rows * 3 + columns;
+    if (gameBoard[index] = ".") {
+        gameBoard[index] = input;
+        player = player % 2 + 1;
+    }
+    else {
+        console.log('You cannot place there');
+    }
+}
+
+checkMatch = (num1, num2, num3) => {
+    if (gameBoard[num1] != "." && gameBoard[num1] == gameBoard[num2] && gameBoard[num2] == gameBoard[num3]) {
+        gameStatus = 0;
+    }
+}
+checkGame = () => {                                 //Function to check wining possibilites
+    checkMatch[0, 1, 2]
+    checkMatch[3, 4, 5],
+        checkMatch[6, 7, 8],
+        checkMatch[0, 3, 6],
+        checkMatch[1, 4, 7],
+        checkMatch[2, 5, 8],
+        checkMatch[0, 4, 8],
+        checkMatch[2, 4, 6]
+}
+computer = () => {                                 //FUnction for generating row and column values
+    row = Math.random() % 3 | 0;
+    column = Math.random() % 3 | 0;
+}
+
 ticTacToeMain = () => {
-    if(player==1){
-        letter="O";
-        console.log("Player player1 trun:",letter);
-        
-    }else{
-        letter="X";
-        console.log("Player player2 trun:",letter);
+    reset();
+    while (true) {
+        if (player == 1) {
+            letter = "O";                          //Letter for to play Game
+            console.log("Player player1 trun:", letter);
+
+        } else {
+            letter = "X";
+            console.log("Player player2 trun:", letter);
+        }
+        print();
+        console.log();
+        console.log("Command");
+        console.log("1 ==>for set[row][column]");
+        console.log('2 ==>for restart');
+        while (true) {
+            if (player == 1) {
+                computer();                                         //calling computer for row and column values
+                selectInput = 1;
+            }
+            if (player == 2) {
+                selectInput = input.questionInt("Enter 1 or 2 :");
+                rows = input.questionInt("Enter row value : ");
+                columns = input.questionInt("Enter column value : ");
+            }
+            if (selectInput == 1) {
+                set(rows, columns, letter);                        //calling set function for adding O or X into board
+                break;
+            } else if (selectInput == 2) {
+                reset();                                            //calling reset to play new game
+                break;
+            }
+            else {
+                console.log("Select 1 or 2");
+            }
+            checkGame();
+            if (gameStatus != 1) {
+                print();
+                console.log("Game over");
+                player = player % 2 + 1;                            //changing player to computer
+                console.log("player", player, letter, "wins");
+                let newGame = input.questionInt("Enter 1 for new match: ");
+                newGame == 1 ? reset() : console.log();
+            }
+        }
     }
 }
